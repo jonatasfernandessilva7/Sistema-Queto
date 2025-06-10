9f68be6deca476d177e88cb7f2b41787e8605bb1
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2025-06-10 10:46:10 -0300

    refs: mudando para vários arquivos serem upados

--- src/backend/controllers/controller_upload.py
+++ src/backend/controllers/controller_upload.py
@@ -1,13 +1,15 @@
-from fastapi import UploadFile, File, HTTPException
+from fastapi import UploadFile, HTTPException, File
+from typing import List
 from src.backend.services.service_upload_arquivo import salvar_arquivo
 
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(file: List[UploadFile] = File(...) ):
 
-    try: 
+    try:
 
         resultado = await salvar_arquivo(file)
+
         return resultado
     
     except Exception as e:
-        
+
         raise HTTPException(status_code=500, detail=str(e))
--- src/backend/services/service_upload_arquivo.py
+++ src/backend/services/service_upload_arquivo.py
@@ -1,18 +1,22 @@
 import os
 import aiofiles 
 
-from fastapi import UploadFile
+from fastapi import UploadFile, File
+from typing import List
 
 UPLOAD_DIR = "../uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-async def salvar_arquivo(file: UploadFile): 
+async def salvar_arquivo(files: List[UploadFile] = File(...)): 
+    uploaded_files_info = []
 
-    file_location = os.path.join(UPLOAD_DIR, file.filename)
-
-    async with aiofiles.open(file_location, "wb") as buffer:
+    for file in files:
+        file_location = os.path.join(UPLOAD_DIR, file.filename)
         
-        while content := await file.read(1024): 
-            await buffer.write(content)
+        async with aiofiles.open(file_location, "wb") as buffer:
+            while content := await file.read(1024): 
+                await buffer.write(content)
+        
+        uploaded_files_info.append({"filename": file.filename, "status": "Arquivo enviado com sucesso."})
 
-    return {"filename": file.filename, "status": "Arquivo enviado com sucesso."}
+    return uploaded_files_info
