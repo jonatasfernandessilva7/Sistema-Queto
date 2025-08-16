8c9f8ccb59ec695f59c10bb67d93ff255262c4e7
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2025-08-16 08:49:10 -0300

    refs names lang

--- src/IA/AiModels.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Dict
-
-class EventModel(BaseModel):
-    
-    type: str
-    origin: str
-    details: Dict[str, str]
--- src/IA/services/AIFeedbackService.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class Feedback(BaseModel):
-    
-    event_id: int 
-    human_classification: str 
-    human_priority: str
-    comment: str = None
